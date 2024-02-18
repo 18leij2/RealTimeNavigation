de8b5c7931cda64915d9b00e688cf7b8e0351206
d-a-t <40614725+d-a-t@users.noreply.github.com>
2024-02-17 23:33:46 -0500

    bleh

---
+++ Assets/Scripts/dat/Room/VRRoom.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace dat
+{
+    public class VRRoom : MonoBehaviour
+    {
+        //protected Transform root = this.gam
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
