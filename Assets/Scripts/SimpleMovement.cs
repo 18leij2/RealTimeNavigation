d90082b87ba8b6a4f12b0d39e857c1f36184414b
18leij2 <jasonleiqi@gmail.com>
2024-02-27 16:08:24 -0500

    added map
    changed player prefab to lock rotation, modified simplemovement script to support oscillation, added experimental map

--- Assets/Scripts/SimpleMovement.cs
+++ Assets/Scripts/SimpleMovement.cs
@@ -5,16 +5,29 @@ public class SimpleMovement : MonoBehaviour
 {
     public float speed = 5f;
     public Vector3 direction = new Vector3(0, 0, 1);
+    public bool oscillate = false;
+    public float targetTime = 5f;
+    private float obstacleTime;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        obstacleTime = targetTime;
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.Translate(direction * speed * Time.deltaTime);
+        
+        if (oscillate)
+        {
+            obstacleTime -= Time.deltaTime;
+            if (obstacleTime <= 0.0f)
+            {
+                direction = -direction;
+                obstacleTime = targetTime;
+            }
+        }
     }
 }
