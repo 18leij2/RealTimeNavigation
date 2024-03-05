da6fbf748a7c6e279e27c170c3bc58a4dbe2b6d4
He, Elise Ying <ehe37@gatech.edu>
2024-03-05 16:01:08 -0500

    random direction for moving obstacle

--- Assets/Scripts/seedGen/Generator.cs
+++ Assets/Scripts/seedGen/Generator.cs
@@ -9,9 +9,17 @@ public class Generator : MonoBehaviour
 
     public int wallObjectsCount = 3;
     public int movingObjectsCount = 2;
-    // Start is called before the first frame update
+    
+    public GameObject player;
+    public Vector3 playerLocation = new Vector3(-2.8f, 1.04f, -2.66f);
+    public GameObject target;
+    public Vector3 targetLocation = new Vector3(0.05f, 0.54f, 0.66f);
+
     void Start()
     {
+        player.transform.position = playerLocation;
+        target.transform.position = targetLocation;
+
         for(int i = 0; i < wallObjectsCount; i++) {
             Vector3 pos = Random.insideUnitSphere * 7;
             pos.y = 20;
@@ -34,6 +42,10 @@ public class Generator : MonoBehaviour
                 pos.y = 1.08f;
                 GameObject selected = movingObjects[Random.Range(0, movingObjects.Length)];
                 GameObject movobs = Instantiate(selected, pos, selected.transform.rotation);
+                SimpleMovement simpleMovementScript = movobs.GetComponent<SimpleMovement>();
+                Vector3 direction = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
+                direction.Normalize();
+                simpleMovementScript.direction = direction;
                 //movobs.direction = Vector3.Normalize(new Vector3(Random.Range(-1, 1), 0, Random.Range(-1, 1)));
                 //movobs.transform.rotation = Quaternion.Euler(-90, Random.Range(0, 360), 0);
             }
